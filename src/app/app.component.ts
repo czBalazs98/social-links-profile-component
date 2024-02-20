608743a7b911cfa739b9b12d9464f8b41f29e58f
bczmarko <bczmarko@intrend.hu>
2024-02-20 13:02:52 +0100

    added assets, social-links-profile component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SocialLinksProfileComponent } from "./social-links-profile/social-links-profile.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SocialLinksProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/social-links-profile/social-links-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-links-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './social-links-profile.component.html',
+  styleUrl: './social-links-profile.component.scss'
+})
+export class SocialLinksProfileComponent {
+
+}
