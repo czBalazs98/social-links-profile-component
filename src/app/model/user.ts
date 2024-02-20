1eaaf82a36b846d00882019acdc9747a6703bdf2
bczmarko <bczmarko@intrend.hu>
2024-02-20 13:21:49 +0100

    added user interface

---
+++ src/app/model/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+  firstName: string;
+  lastName: string;
+  country: string;
+  city: string;
+  introduction: string;
+  gitHubUrl: string | null;
+  frontendMentorUrl: string | null;
+  linkedInUrl: string | null;
+  twitterUrl: string | null;
+  instagramUrl: string | null;
+}
