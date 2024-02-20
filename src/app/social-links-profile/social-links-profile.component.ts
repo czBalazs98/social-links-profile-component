ec9c8edbaf0db95b94221a10691da2d8ef821df4
bczmarko <bczmarko@intrend.hu>
2024-02-20 13:41:34 +0100

    social-links-profile component skeleton

--- src/app/social-links-profile/social-links-profile.component.ts
+++ src/app/social-links-profile/social-links-profile.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-social-links-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './social-links-profile.component.html',
   styleUrl: './social-links-profile.component.scss'
 })
