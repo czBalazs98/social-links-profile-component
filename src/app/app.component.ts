4a50c12e23cfda89ee57574ed26b9a90b415d8a8
bczmarko <bczmarko@intrend.hu>
2024-02-20 13:16:42 +0100

    base styles, footer component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SocialLinksProfileComponent } from "./social-links-profile/social-links-profile.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SocialLinksProfileComponent],
+  imports: [RouterOutlet, SocialLinksProfileComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
